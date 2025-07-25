8b73486431b1e26450837d6cc5c4f3c655ce8ee9
laurie <lauriegauth@gmail.com>
2025-07-25 19:41:42 +0200

    Ajout de la possibilitée d'ajout de projets

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Project;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Employe;
 
 final class ProjectController extends AbstractController
 {
@@ -22,6 +23,18 @@ final class ProjectController extends AbstractController
         // Passer les projets au template
         return $this->render('project/index.html.twig', [
             'projects' => $projects,
+        ]);
+    }
+    #[Route('/project/add', name: 'project_add')]
+    public function addProject(ManagerRegistry $doctrine): Response
+    {
+        // Récupérer le repository pour l'entité Project
+        $employeRepository = $doctrine->getRepository(Employe::class);
+        // Récupérer tous les employés
+        $employes = $employeRepository->findAll();
+        // Passer les emplyés au template
+        return $this->render('project/project_add.html.twig', [
+            'employes' => $employes,
         ]);
     }
 }
