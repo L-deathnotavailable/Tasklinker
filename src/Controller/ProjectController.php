2c3bd85445d03aa5e2bd9c378b4c8fc34a9e1a02
laurie <lauriegauth@gmail.com>
2025-07-25 15:42:04 +0200

    Mise en dynamique de l'index

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Project;
+use Doctrine\Persistence\ManagerRegistry;
 
 final class ProjectController extends AbstractController
 {
     #[Route('/', name: 'app_project')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        // Récupérer le repository pour l'entité Project
+         $projectRepository = $doctrine->getRepository(Project::class);
+
+        // Récupérer tous les projets
+        $projects = $projectRepository->findAll();
+
+        // Passer les projets au template
         return $this->render('project/index.html.twig', [
-            'controller_name' => 'ProjectController',
+            'projects' => $projects,
         ]);
     }
 }
