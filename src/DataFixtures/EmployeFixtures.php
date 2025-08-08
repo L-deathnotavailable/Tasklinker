258a2e2a0d3b358ad915c2a36af64a217a3c5d82
laurie <lauriegauth@gmail.com>
2025-08-08 16:52:27 +0200

    ajout de fixtures des emails des employes

--- src/DataFixtures/EmployeFixtures.php
+++ src/DataFixtures/EmployeFixtures.php
@@ -11,15 +11,16 @@ class EmployeFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $employesData = [
-            ['name' => 'Dupont', 'firstname' => 'Jean', 'levelAccess' => 'Admin', 'ContractType' => 'CDI', 'ArrivedDate' => new \DateTime('2020-01-15')],
-            ['name' => 'Martin', 'firstname' => 'Marie', 'levelAccess' => 'User', 'ContractType' => 'CDD', 'ArrivedDate' => new \DateTime('2021-03-22')],
-            ['name' => 'Bernard', 'firstname' => 'Luc', 'levelAccess' => 'User', 'ContractType' => 'CDI', 'ArrivedDate' => new \DateTime('2019-11-05')],
+            ['name' => 'Dupont', 'firstname' => 'Jean', 'email' => 'jean.dupont@driblet.com', 'levelAccess' => 'Admin', 'ContractType' => 'CDI', 'ArrivedDate' => new \DateTime('2020-01-15')],
+            ['name' => 'Martin', 'firstname' => 'Marie', 'email' => 'marie.martin@driblet.com', 'levelAccess' => 'User', 'ContractType' => 'CDD', 'ArrivedDate' => new \DateTime('2021-03-22')],
+            ['name' => 'Bernard', 'firstname' => 'Luc', 'email' => 'luc.bernard@driblet.com', 'levelAccess' => 'User', 'ContractType' => 'CDI', 'ArrivedDate' => new \DateTime('2019-11-05')],
         ];
 
         foreach ($employesData as $data) {
             $employe = new Employe();
             $employe->setName($data['name']);
             $employe->setFirstname($data['firstname']);
+            $employe->setEmail($data['email']);
             $employe->setLevelAccess($data['levelAccess']);
             $employe->setContractType($data['ContractType']);
             $employe->setArrivedDate($data['ArrivedDate']);
